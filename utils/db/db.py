a9098ecb0575d27a0ddbbe7c6e3a4eeefcd6762e
artem Gromov <gromartem2000@gmail.com>
2020-07-01 15:03:40 +0300

    better table craeate

--- utils/db/db.py
+++ utils/db/db.py
@@ -3,7 +3,7 @@ import sqlite3
 #Декоратор для безопасного подключения к базе данных
 def ensure_connection(func):
     def inner(*args, **kwargs):
-        with sqlite3.connect('utils/db/tempData.db') as conn:
+        with sqlite3.connect('utils/db/tempHumData.db') as conn:
             print("connection success")
             kwargs['conn'] = conn
             res = func(*args, **kwargs)
@@ -17,15 +17,15 @@ def init_db(conn, force: bool = False): #force - пересоздание таб
     c = conn.cursor()
     if force:
         c.execute('DROP TABLE IF EXISTS temp_hum_data')
-
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS temp_hum_data (
-            id          INTEGER PRIMARY KEY,
-            time        TEXT NOT NULL,
-            tempr        TEXT NOT NULL,
-            hum         TEXT NOT NULL
-        )
-    ''')
+    
+    creat_table = """CREATE TABLE IF NOT EXISTS temp_hum_data (
+                        id          INTEGER PRIMARY KEY,
+                        Time           text NOT NULL,
+                        Temperature,C  text NOT NULL,
+                        Humidity,%     text NOT NULL
+                        
+                    );"""     
+    c.execute(creat_table)
     # Сохранить изменения
     conn.commit()
     print("db created or allready exists")
@@ -33,5 +33,5 @@ def init_db(conn, force: bool = False): #force - пересоздание таб
 @ensure_connection
 def add_data(conn, time: str, temp: str, hum: str):
     c = conn.cursor()
-    c.execute('INSERT INTO temp_hum_data (time, tempr, hum) VALUES(?,?,?)', (time, temp, hum))
+    c.execute('INSERT INTO temp_hum_data (Time, Temperature,C, Humidity,%) VALUES(?,?,?)', (time, temp, hum))
     conn.commit()
