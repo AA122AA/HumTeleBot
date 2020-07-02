2051564e53ab164d906071f82360ff1f7c2199e9
AA122AA <gromartem2000@gmail.com>
2020-07-02 16:18:20 +0300

    more log

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from handlers import bot 
 
+with open("/home/pi/log.txt", 'a') as f:
+    f.write("import handler.bot done \n")
+
 def main():
     print("bot started")
+    with open("/home/pi/log.txt", 'a') as f:
+        f.write("bot started \n")
     bot.polling(none_stop=True)
 
 if __name__ == "__main__":
