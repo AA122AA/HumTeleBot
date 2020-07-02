521d98a4e27922e66e8c5cf7f04676def35b03be
AA122AA <gromartem2000@gmail.com>
2020-07-02 16:10:01 +0300

    add more log

--- loader.py
+++ loader.py
@@ -1,9 +1,14 @@
 import telebot
 
 with open("/home/pi/stop.txt", 'w') as f:
-    f.write("import done")
+    f.write("import telebot done \n")
 from data.config import TOKEN
 from utils.db.db import init_db
 
+with open("/home/pi/stop.txt", 'w') as f:
+    f.write("import Token and init_db done \n")
+
 init_db()
+with open("/home/pi/stop.txt", 'w') as f:
+    f.write("init_db done \n")
 bot = telebot.TeleBot(TOKEN)
