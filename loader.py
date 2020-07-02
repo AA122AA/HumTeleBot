30a9624ab5415831803e67b874a143d011826224
AA122AA <gromartem2000@gmail.com>
2020-07-02 16:16:42 +0300

    w->a

--- loader.py
+++ loader.py
@@ -1,14 +1,14 @@
 import telebot
 
-with open("/home/pi/stop.txt", 'w') as f:
+with open("/home/pi/log.txt", 'a') as f:
     f.write("import telebot done \n")
 from data.config import TOKEN
 from utils.db.db import init_db
 
-with open("/home/pi/stop.txt", 'w') as f:
+with open("/home/pi/log.txt", 'a') as f:
     f.write("import Token and init_db done \n")
 
 init_db()
-with open("/home/pi/stop.txt", 'w') as f:
+with open("/home/pi/log.txt", 'a') as f:
     f.write("init_db done \n")
 bot = telebot.TeleBot(TOKEN)
