a22d18de86b7f55572239801c89b4ee608edea39
AA122AA <gromartem2000@gmail.com>
2020-07-02 15:56:53 +0300

    add requirements

--- loader.py
+++ loader.py
@@ -1,4 +1,9 @@
-import telebot
+try:
+    import telebot
+except ModuleNotFoundError as e:
+    with open("/home/pi/error.txt", 'w') as f:
+        f.write(e)
+        f.write("kek")
 from data.config import TOKEN
 from utils.db.db import init_db
 
