98eb9202033abcb22a06b24f1db05212429755e3
AA122AA <gromartem2000@gmail.com>
2020-07-02 16:55:06 +0300

    rm log

--- app.py
+++ app.py
@@ -1,25 +1,8 @@
 from handlers import bot 
 
-with open("/home/pi/log.txt", 'a') as f:
-    f.write("import handler.bot done \n")
-
 def main():
     print("bot started")
-    with open("/home/pi/log.txt", 'a') as f:
-        f.write("bot started \n")
-    try:
-        bot.polling(none_stop=True)     
-    except Exception as e:
-        with open("/home/pi/log.txt", 'a') as f:
-            f.write(str(e) + "\n")
-        with open("/home/pi/log.txt", 'a') as f:
-            f.write("module not found \n")
-    except:
-        with open("/home/pi/log.txt", 'a') as f:
-            f.write("bot drop polling \n")
-
-    with open("/home/pi/log.txt", 'a') as f:
-       f.write("bot polling \n")
+    bot.polling(none_stop=True)      
 
 if __name__ == "__main__":
     main()
