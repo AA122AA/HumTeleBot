ccb318b2cd15c3ae42fbf33759b20054ddc175f7
AA122AA <gromartem2000@gmail.com>
2020-07-02 15:48:09 +0300

    add exept module

--- loader.py
+++ loader.py
@@ -1,4 +1,8 @@
-import telebot
+try:
+    import telebot
+except ModuleNotFoundError as e:
+    with open("/home/pi/error.txt", 'w') as f:
+        f.write(e)
 from data.config import TOKEN
 from utils.db.db import init_db
 
