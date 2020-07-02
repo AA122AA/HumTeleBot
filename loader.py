a32828c1b190166ca15d07e3515a8668b6262fe0
AA122AA <gromartem2000@gmail.com>
2020-07-02 15:49:18 +0300

    add exept

--- loader.py
+++ loader.py
@@ -3,6 +3,7 @@ try:
 except ModuleNotFoundError as e:
     with open("/home/pi/error.txt", 'w') as f:
         f.write(e)
+        f.write("kek")
 from data.config import TOKEN
 from utils.db.db import init_db
 
